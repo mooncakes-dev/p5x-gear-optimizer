849950f403b997ed7e9b9bfb76670732289a369f
mooncakes <mneshenko@gmail.com>
2025-12-12 12:05:00 -0500

    adding dark mode toggle, linters, prettier

--- p5x-gear-optimizer/src/app/app.ts
+++ p5x-gear-optimizer/src/app/app.ts
@@ -1,12 +1,15 @@
-import { Component, signal } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HlmButtonImports } from '@spartan-ng/helm/button';
 import { HlmSidebarImports } from '@spartan-ng/helm/sidebar';
 import { AppSidebar } from './shared/components/sidebar/sidebar.component';
+import { ModeToggle } from './shared/components/mode-toggle/mode-toggle';
+import { C } from 'node_modules/@angular/cdk/focus-monitor.d-CvvJeQRc';
 
 @Component({
   selector: 'app-root',
-  imports: [AppSidebar, HlmSidebarImports, RouterOutlet],
+  imports: [AppSidebar, HlmSidebarImports, RouterOutlet, ModeToggle],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ p5x-gear-optimizer/src/app/shared/components/mode-toggle/mode-toggle.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ModeToggle } from './mode-toggle';
+
+describe('ModeToggle', () => {
+  let component: ModeToggle;
+  let fixture: ComponentFixture<ModeToggle>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ModeToggle]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ModeToggle);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
